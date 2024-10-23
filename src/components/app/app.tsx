8c28575e228c87dd6a564648c839e6f55f335463
Alexandr Sergeev <ntl1221@yandex.ru>
2024-10-23 22:46:47 +0300

    v.2.8

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -68,6 +68,7 @@ const App = () => {
         />
         <Route path='/ingredients/:id' element={<IngredientDetails />} />
         <Route path='/feed/:number' element={<OrderInfo />} />
+        <Route path='/profile/orders/:number' element={<ProtectedRoute><OrderInfo /></ProtectedRoute>} />
         <Route path='*' element={<NotFound404 />} />
       </Routes>
 
