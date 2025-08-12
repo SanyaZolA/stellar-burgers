8b0b1f27cdb1ce68499156da9041c12593dd8b0c
Alexandr <ntl1221@yandex.ru>
2025-08-12 17:30:46 +0300

    fix

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -50,7 +50,7 @@ const App = () => {
           <Route path='/' element={<ConstructorPage />} />
           <Route path='/feed' element={<Feed />} />
           <Route path='/login' element={<Login />} />
-
+          <Route path='/register' element={<Register />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
           <Route path='/reset-password' element={<ResetPassword />} />
           <Route
